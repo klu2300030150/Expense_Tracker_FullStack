b9e6b98018b75f9d24bdabd84f5ddfa16f47e101
Dacharla Sreekar <student-01-e7114bd68014@qwiklabs.net>
2025-10-11 12:28:06 +0530

    fix: robust context usage in main.jsx to prevent blank page on deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { useApp, AppProvider } from './state/AppContext.jsx'
-
-function Root() {
-  const { state } = useApp();
-  return (
-    <div data-panic={state.settings.panicHide ? 'true' : 'false'}>
-      <App />
-    </div>
-  );
-}
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AppProvider } from './state/AppContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
-      <Root />
+      <App />
     </AppProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
